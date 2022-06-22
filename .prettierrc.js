970d5c1869bdd14a4d8bb75e7d46d151e285e189
沫俱宏 <negivup@sina.com>
2022-06-22 21:38:03 +0800

    初始化项目

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: 'lf',
+  useTabs: false,
+  tabWidth: 2,
+  arrowParens: 'always',
+  trailingComma: 'all',
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  bracketSameLine: false,
+  vueIndentScriptAndStyle: false,
+};
